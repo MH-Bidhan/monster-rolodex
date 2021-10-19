3e25e3f91816823be5a2d74e152ff9560fdd4b0e
MH-Bidhan <mdbidhan55@gmail.com>
2021-10-19 21:13:21 +0600

    Added home page for the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Route, Switch } from "react-router";
+import MonsterRolodex from "./pages/monster-rolodex.component";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={MonsterRolodex} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/monster-rolodex.component.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./monster-rolodex.styles.scss";
+
+class MonsterRolodex extends React.Component {
+  render() {
+    return (
+      <div className="home">
+        <div className="container">
+          <h1 className="logo">Monster Rolodex</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MonsterRolodex;
